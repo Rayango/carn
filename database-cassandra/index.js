09818e99972e9c82852998236b13a1c06a69f39d
Ryan Ngo <rayango92@gmail.com>
2018-01-30 14:58:47 -0800

    Generate 10M lines of data and insert into database

--- database-cassandra/index.js
+++ database-cassandra/index.js
@@ -1,2 +1,13 @@
 const cassandra = require('cassandra-driver');
+const client = new cassandra.Client({ contactPoints: ['127.0.0.1'], keyspace: 'events' });
+console.log('connecting client....', client);
+const query = `CREATE TABLE views (
+  rate float,
+  price float,
+  zip text,
+  time text,
+  PRIMARY KEY (rate, zip, time)
+)`;
 
+client.execute(query)
+  .then(result => console.log('result...', result));
--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('koa-router');
+const fs = require('fs');
+const generateFakeData = require('../fakeData/fakeDataGenerator.js');
 
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
-});
+const app = new Koa();
+const router = new Router();
+
+router
+  .get('/', (ctx, next) => {
+    ctx.body = 'Hello Koa!';
+  });
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(3000);
