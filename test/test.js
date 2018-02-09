8aa6d8a267813832bdabf361aaec947172d19786
Ryan Ngo <rayango92@gmail.com>
2018-02-09 12:32:06 -0800

    Add test for data generation

--- test/test.js
+++ test/test.js
@@ -5,41 +5,42 @@ const app = require('../server/index.js');
 const supertest = require('supertest');
 const db = require('../database-cassandra/index.js');
 const moment = require('moment');
+const fakeDataGenerator = require('../testData/fakeDataGenerator.js');
 
 const request = supertest.agent(app.listen());
 
 describe('server', function() {
-  it('should respond with a greeting when the index page is visited', (done) => {
-    request
-      .get('/')
-      .expect(200)
-      .end((err, res) => {
-        assert.equal(res.body.data, 'Hello Koa!');
-        done();
-      });
-  }); 
+  // it('should respond with a greeting when the index page is visited', (done) => {
+  //   request
+  //     .get('/')
+  //     .expect(200)
+  //     .end((err, res) => {
+  //       assert.equal(res.body.data, 'Hello Koa!');
+  //       done();
+  //     });
+  // }); 
 
-  it('should have a route called /requests to take requests from the Passengers API', function(done) {
-    let timestamp = new Date(new Date() - (Math.random() * 8.64e7)).toISOString().split('.')[0]+"-0800";
-    let data = {
-      id: uuid(),
-      rate: 2.65, 
-      zipOrigin: 94105,
-      zipDestination: 94122,
-      timestamp: timestamp,
-      // hourBucket: moment(timestamp).format('MMMM Do YYYY h a'),
-      // minuteBucket: moment(timestamp).format('MMMM Do YYYY h:mm a'),
-      price: 6.85,
-      ride: false
-    };
+  // it('should have a route called /requests to take requests from the Passengers API', function(done) {
+  //   let timestamp = new Date(new Date() - (Math.random() * 8.64e7)).toISOString().split('.')[0]+"-0800";
+  //   let data = {
+  //     id: uuid(),
+  //     rate: 2.65, 
+  //     zipOrigin: 94105,
+  //     zipDestination: 94122,
+  //     timestamp: timestamp,
+  //     // hourBucket: moment(timestamp).format('MMMM Do YYYY h a'),
+  //     // minuteBucket: moment(timestamp).format('MMMM Do YYYY h:mm a'),
+  //     price: 6.85,
+  //     ride: false
+  //   };
 
-    request
-      .post('/requests')
-      .send(data)
-      .end((err, res) => {
-        done();
-      });  
-  });
+  //   request
+  //     .post('/requests')
+  //     .send(data)
+  //     .end((err, res) => {
+  //       done();
+  //     });  
+  // });
 
   it('should return aggregated data when dataForFares endpoint is visited', function(done) {
     request
@@ -47,7 +48,6 @@ describe('server', function() {
       .expect(200)
       .end((err, res) => {
         assert.equal(res.body.data.length, 27);
-        console.log(res.body.data);
         for (let zipCodeData of res.body.data) {
           assert.isNumber(+zipCodeData.rides);
         }
@@ -55,3 +55,10 @@ describe('server', function() {
       });
   });
 });
+
+// describe('fake data generator', function() {
+//   it('should return an array with 6 properties', function(done) {
+//     assert.equal(fakeDataGenerator().length, 6);
+//     done();
+//   });
+// });  
